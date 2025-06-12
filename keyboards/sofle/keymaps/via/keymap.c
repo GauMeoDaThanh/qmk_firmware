f50732ad78b10d471963ab91b75cfd0af1ce2a93
unknown <ddat888@gmail.com>
2025-06-12 21:04:40 +0700

    custom qmk sofle profile

---
+++ keyboards/sofle/keymaps/via/keymap.c
@@ -0,0 +1,182 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+#include QMK_KEYBOARD_H
+
+enum sofle_layers {
+    /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
+    _QWERTY,
+    _Nav,
+    _Func,
+    _Syntax
+};
+
+enum custom_keycodes {
+    KC_PRVWD = QK_USER,
+    KC_NXTWD,
+    KC_LSTRT,
+    KC_LEND
+};
+
+const uint16_t PROGMEM esc[] = {KC_GRV, KC_1, COMBO_END};
+const uint16_t PROGMEM func[] = {KC_LGUI, KC_LALT, KC_LCTL, COMBO_END};
+
+
+combo_t key_combos[] = {
+    COMBO(esc, KC_ESC),
+    COMBO(func, TG(_Func)),
+};
+
+#define KC_QWERTY PDF(_QWERTY)
+#define KC_COLEMAK PDF(_COLEMAK)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/*
+ * QWERTY
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Caps |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            | LGUI | LAlt | LCTR |Space | /  _Nav /       \ _Nav \  |Enter | RCTR | RAlt | RGUI |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            `----------------------------------'           '------''---------------------------'
+ */
+
+[_QWERTY] = LAYOUT(
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
+  KC_CAPS,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_PSCR,     KC_MUTE,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+                KC_LGUI,KC_LALT,KC_LCTL, LT(_Syntax, KC_SPC), MO(_Nav),      MO(_Nav),  KC_ENT, KC_RCTL, KC_RALT, KC_RGUI
+),
+/* Nav
+ * ,----------------------------------------.                     ,--------------------------------------------.
+ * |      |      |      |      |      |      |                    |         |      |      |      |       |     |
+ * |------+------+------+------+------+------|                    |---------+------+------+------+------+------|
+ * | Esc  | Ins  | Pscr | Menu |      |      |                    |Page up  | PWrd |  Up  | NWrd | DLine| Bspc |
+ * |------+------+------+------+------+------|                    |---------+------+------+------+------+------|
+ * | Tab  | LAt  | LCtl |LShift|      | Caps |-------.     ,-------|Page down| Left | Down | Rigth|  Del | Bspc |
+ * |------+------+------+------+------+------|  MUTE  |   |      |---------+------+------+------+------+------|
+ * | Redo | Undo |  Cut | Copy | Paste|      |-------|    |-------|         | LStr |      | LEnd |      | Shift|
+ * `-----------------------------------------/       /     \      \--------------------------------------------'
+ *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            `----------------------------------'           '------''---------------------------'
+ */
+[_Nav] = LAYOUT(
+  _______, S(KC_Q) , S(KC_W) , S(KC_E) , S(KC_R) , _______,                           _______,  _______  , _______,  _______ ,  _______ ,_______,
+  _______,  KC_INS,  KC_PSCR,   KC_APP,  XXXXXXX, XXXXXXX,                        KC_PGUP, KC_PRVWD,   KC_UP, KC_NXTWD,C(KC_BSPC), KC_BSPC,
+  C(KC_Y), KC_LALT,  KC_LCTL,  KC_LSFT,  XXXXXXX, KC_CAPS,                       KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
+  _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XXXXXXX,  _______,       _______,  XXXXXXX, KC_LSTRT, XXXXXXX, KC_LEND,   XXXXXXX, _______,
+                         _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
+),
+/* Func
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |    |  F7  |  F8  |  F9  | F10  | F11  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |  `   |   1  |   2  |   3  |   4  |   5  |                    |     |   7  |   8  |   9  |   0  | F12  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|     |   &  |   *  |   (  |   )  |   |  |
+ * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
+ * | Shift|  =   |  -   |  +   |   {  |   }  |-------|    |-------|     |   ]  |   ;  |   :  |   \  | Shift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            `----------------------------------'           '------''---------------------------'
+ */
+[_Func] = LAYOUT(KC_NO, KC_NO, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSLS, KC_NO, KC_NO, KC_NO, KC_NO, KC_F7, KC_F8, KC_F9, KC_NO, KC_P7, KC_P8, KC_P9, KC_PAST, KC_NO, KC_NO, KC_NO, KC_NO, KC_F4, KC_F5, KC_F6, KC_NO, KC_P4, KC_P5, KC_P6, KC_PMNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_NO, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_PPLS, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P0, KC_PDOT, KC_PENT, KC_NO),
+[_Syntax] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_EQL, KC_LPRN, KC_RPRN, KC_NO, KC_NO, KC_BSLS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UNDS, KC_NO, KC_NO, KC_LBRC, KC_RBRC, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [1] = { ENCODER_CCW_CW(UG_HUED, UG_HUEU),  ENCODER_CCW_CW(UG_SATD, UG_SATU)  },
+    [2] = { ENCODER_CCW_CW(UG_VALD, UG_VALU),  ENCODER_CCW_CW(UG_SPDD, UG_SPDU)  },
+    [3] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+};
+#endif
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_PRVWD:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                    register_mods(mod_config(MOD_LALT));
+                    register_code(KC_LEFT);
+                } else {
+                    register_mods(mod_config(MOD_LCTL));
+                    register_code(KC_LEFT);
+                }
+            } else {
+                if (keymap_config.swap_lctl_lgui) {
+                    unregister_mods(mod_config(MOD_LALT));
+                    unregister_code(KC_LEFT);
+                } else {
+                    unregister_mods(mod_config(MOD_LCTL));
+                    unregister_code(KC_LEFT);
+                }
+            }
+            break;
+        case KC_NXTWD:
+             if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                    register_mods(mod_config(MOD_LALT));
+                    register_code(KC_RIGHT);
+                } else {
+                    register_mods(mod_config(MOD_LCTL));
+                    register_code(KC_RIGHT);
+                }
+            } else {
+                if (keymap_config.swap_lctl_lgui) {
+                    unregister_mods(mod_config(MOD_LALT));
+                    unregister_code(KC_RIGHT);
+                } else {
+                    unregister_mods(mod_config(MOD_LCTL));
+                    unregister_code(KC_RIGHT);
+                }
+            }
+            break;
+        case KC_LSTRT:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                     //CMD-arrow on Mac, but we have CTL and GUI swapped
+                    register_mods(mod_config(MOD_LCTL));
+                    register_code(KC_LEFT);
+                } else {
+                    register_code(KC_HOME);
+                }
+            } else {
+                if (keymap_config.swap_lctl_lgui) {
+                    unregister_mods(mod_config(MOD_LCTL));
+                    unregister_code(KC_LEFT);
+                } else {
+                    unregister_code(KC_HOME);
+                }
+            }
+            break;
+        case KC_LEND:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                    //CMD-arrow on Mac, but we have CTL and GUI swapped
+                    register_mods(mod_config(MOD_LCTL));
+                    register_code(KC_RIGHT);
+                } else {
+                    register_code(KC_END);
+                }
+            } else {
+                if (keymap_config.swap_lctl_lgui) {
+                    unregister_mods(mod_config(MOD_LCTL));
+                    unregister_code(KC_RIGHT);
+                } else {
+                    unregister_code(KC_END);
+                }
+            }
+            break;
+    }
+    return true;
+}
--- keyboards/sofle/sofle.c
+++ keyboards/sofle/sofle.c
@@ -4,41 +4,32 @@
 
 #ifdef SWAP_HANDS_ENABLE
 
-__attribute__ ((weak))
-const keypos_t PROGMEM hand_swap_config[MATRIX_ROWS][MATRIX_COLS] =
+__attribute__((weak)) const keypos_t PROGMEM hand_swap_config[MATRIX_ROWS][MATRIX_COLS] =
     // The LAYOUT macro could work for this, but it was harder to figure out the
     // identity using it.
 
     // This is the identity layout.
-/*
-{
-    { {0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0} },
-    { {0, 1}, {1, 1}, {2, 1}, {3, 1}, {4, 1}, {5, 1} },
-    { {0, 2}, {1, 2}, {2, 2}, {3, 2}, {4, 2}, {5, 2} },
-    { {0, 3}, {1, 3}, {2, 3}, {3, 3}, {4, 3}, {5, 3} },
-    { {0, 4}, {1, 4}, {2, 4}, {3, 4}, {4, 4}, {5, 4} },
+    /*
+    {
+        { {0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0} },
+        { {0, 1}, {1, 1}, {2, 1}, {3, 1}, {4, 1}, {5, 1} },
+        { {0, 2}, {1, 2}, {2, 2}, {3, 2}, {4, 2}, {5, 2} },
+        { {0, 3}, {1, 3}, {2, 3}, {3, 3}, {4, 3}, {5, 3} },
+        { {0, 4}, {1, 4}, {2, 4}, {3, 4}, {4, 4}, {5, 4} },
 
-    { {0, 5}, {1, 5}, {2, 5}, {3, 5}, {4, 5}, {5, 5} },
-    { {0, 6}, {1, 6}, {2, 6}, {3, 6}, {4, 6}, {5, 6} },
-    { {0, 7}, {1, 7}, {2, 7}, {3, 7}, {4, 7}, {5, 7} },
-    { {0, 8}, {1, 8}, {2, 8}, {3, 8}, {4, 8}, {5, 8} },
-    { {0, 9}, {1, 9}, {2, 9}, {3, 9}, {4, 9}, {5, 9} },
-};
-*/
+        { {0, 5}, {1, 5}, {2, 5}, {3, 5}, {4, 5}, {5, 5} },
+        { {0, 6}, {1, 6}, {2, 6}, {3, 6}, {4, 6}, {5, 6} },
+        { {0, 7}, {1, 7}, {2, 7}, {3, 7}, {4, 7}, {5, 7} },
+        { {0, 8}, {1, 8}, {2, 8}, {3, 8}, {4, 8}, {5, 8} },
+        { {0, 9}, {1, 9}, {2, 9}, {3, 9}, {4, 9}, {5, 9} },
+    };
+    */
 
     // This is the mirror, q <-> p, w <-> o, etc...
-{
- { {0, 5}, {1, 5}, {2, 5}, {3, 5}, {4, 5}, {5, 5} },
- { {0, 6}, {1, 6}, {2, 6}, {3, 6}, {4, 6}, {5, 6} },
- { {0, 7}, {1, 7}, {2, 7}, {3, 7}, {4, 7}, {5, 7} },
- { {0, 8}, {1, 8}, {2, 8}, {3, 8}, {4, 8}, {5, 8} },
- { {0, 9}, {1, 9}, {2, 9}, {3, 9}, {4, 9}, {5, 9} },
+    {
+        {{0, 5}, {1, 5}, {2, 5}, {3, 5}, {4, 5}, {5, 5}}, {{0, 6}, {1, 6}, {2, 6}, {3, 6}, {4, 6}, {5, 6}}, {{0, 7}, {1, 7}, {2, 7}, {3, 7}, {4, 7}, {5, 7}}, {{0, 8}, {1, 8}, {2, 8}, {3, 8}, {4, 8}, {5, 8}}, {{0, 9}, {1, 9}, {2, 9}, {3, 9}, {4, 9}, {5, 9}},
 
- { {0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0} },
- { {0, 1}, {1, 1}, {2, 1}, {3, 1}, {4, 1}, {5, 1} },
- { {0, 2}, {1, 2}, {2, 2}, {3, 2}, {4, 2}, {5, 2} },
- { {0, 3}, {1, 3}, {2, 3}, {3, 3}, {4, 3}, {5, 3} },
- { {0, 4}, {1, 4}, {2, 4}, {3, 4}, {4, 4}, {5, 4} },
+        {{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}}, {{0, 1}, {1, 1}, {2, 1}, {3, 1}, {4, 1}, {5, 1}}, {{0, 2}, {1, 2}, {2, 2}, {3, 2}, {4, 2}, {5, 2}}, {{0, 3}, {1, 3}, {2, 3}, {3, 3}, {4, 3}, {5, 3}}, {{0, 4}, {1, 4}, {2, 4}, {3, 4}, {4, 4}, {5, 4}},
 };
 
 #    ifdef ENCODER_MAP_ENABLE
@@ -56,12 +47,11 @@ oled_rotation_t oled_init_kb(oled_rotation_t rotation) {
 }
 
 static void render_logo(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-        0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-        0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0
-    };
-    oled_write_P(qmk_logo, false);
+    // static const char PROGMEM qmk_logo[] = {0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94, 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4, 0};
+    // oled_write_P(qmk_logo, false);
+    oled_write_P(PSTR("\n"), false);
+    oled_write_P(PSTR("GauMeoDaThanh\n\n"), false);
+    oled_write_P(PSTR("Sofle v1.0 Keyboard\n"), false);
 }
 
 void print_status_narrow(void) {
@@ -70,8 +60,8 @@ void print_status_narrow(void) {
         case 0:
             oled_write_ln_P(PSTR("Qwrt"), false);
             break;
-        case 1:
-            oled_write_ln_P(PSTR("Clmk"), false);
+        case 2:
+            oled_write_ln_P(PSTR("Func"), false);
             break;
         default:
             oled_write_P(PSTR("Mod\n"), false);
@@ -81,20 +71,16 @@ void print_status_narrow(void) {
     oled_write_ln_P(PSTR("LAYER"), false);
     switch (get_highest_layer(layer_state)) {
         case 0:
-        case 1:
             oled_write_P(PSTR("Base\n"), false);
             break;
-        case 2:
-            oled_write_P(PSTR("Lower"), false);
+        case 1:
+            oled_write_P(PSTR("Nav\n"), false);
             break;
         case 3:
-            oled_write_P(PSTR("Raise"), false);
-            break;
-        case 4:
-            oled_write_P(PSTR("Adjust"), false);
+            oled_write_P(PSTR("Syntax\n"), false);
             break;
         default:
-            oled_write_ln_P(PSTR("Undef"), false);
+            break;
     }
     oled_write_P(PSTR("\n\n"), false);
     led_t led_usb_state = host_keyboard_led_state();
@@ -129,8 +115,14 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
     } else if (index == 1) {
         if (clockwise) {
             tap_code(KC_PGDN);
+            tap_code(KC_PGDN);
+            tap_code(KC_PGDN);
+
+
         } else {
             tap_code(KC_PGUP);
+            tap_code(KC_PGUP);
+            tap_code(KC_PGDN);
         }
     }
     return true;
