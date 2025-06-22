7be4799af24e48e2687283594d206ff4286ee5c1
unknown <ddat888@gmail.com>
2025-06-22 21:34:37 +0700

    keycode for use mouse

--- keyboards/sofle/keymaps/via/config.h
+++ keyboards/sofle/keymaps/via/config.h
@@ -2,3 +2,8 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
+
+#define MOUSEKEY_MOVE_DELTA 5        // Default is 8
+#define MOUSEKEY_MAX_SPEED 4        // Default is 10
+#define MOUSEKEY_TIME_TO_MAX 30      // Default is 30
+#define MOUSEKEY_INTERVAL 16
