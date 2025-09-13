73af738a645f266de7b34ed4f2276fc1f201fdf0
unknown <ddat888@gmail.com>
2025-09-13 14:41:19 +0700

    new keymap version

--- keyboards/sofle/keymaps/via/keymap.c
+++ keyboards/sofle/keymaps/via/keymap.c
@@ -26,9 +26,9 @@ const uint16_t PROGMEM esc[] = {KC_GRV, KC_1, COMBO_END};
 const uint16_t PROGMEM func[] = {KC_LGUI, KC_LALT, KC_LCTL, COMBO_END};
 const uint16_t PROGMEM equal[] = {KC_J, KC_K, COMBO_END};
 const uint16_t PROGMEM left_brace[] = {KC_D, KC_F, COMBO_END};
-const uint16_t PROGMEM right_brace[] = {KC_D, KC_G, COMBO_END};
+const uint16_t PROGMEM right_brace[] = {KC_F, KC_G, COMBO_END};
 const uint16_t PROGMEM left_bracket[] = {KC_C, KC_V, COMBO_END};
-const uint16_t PROGMEM right_bracket[] = {KC_C, KC_B, COMBO_END};
+const uint16_t PROGMEM right_bracket[] = {KC_V, KC_B, COMBO_END};
 const uint16_t PROGMEM backslash[] = {KC_M, KC_COMM, COMBO_END};
 
 combo_t key_combos[] = {
@@ -84,9 +84,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_Nav] = LAYOUT(
-  A(KC_F4), KC_WDELDESK , KC_WDESK_L, KC_WTAB,  KC_WDESK_R , KC_WNEWDESK,                                                     KC_WNEWDESK,KC_WDESK_L, KC_WTAB,  KC_WDESK_R,  KC_WDELDESK ,A(KC_F4),
-  _______,  KC_INS,  QK_MOUSE_BUTTON_1,   QK_MOUSE_CURSOR_UP,  QK_MOUSE_BUTTON_2, XXXXXXX,                            KC_PGUP, KC_PRVWD,   KC_UP, KC_NXTWD,C(KC_BSPC), KC_BSPC,
-  C(KC_Y), C(KC_A),  QK_MOUSE_CURSOR_LEFT,  QK_MOUSE_CURSOR_DOWN,  QK_MOUSE_CURSOR_RIGHT, KC_CAPS,                  KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
+  _______, A(KC_TAB) , KC_WDESK_L, KC_WTAB,  KC_WDESK_R , KC_WNEWDESK,                                                     KC_WNEWDESK,KC_WDESK_L, KC_WTAB,  KC_WDESK_R,  KC_WDELDESK ,A(KC_F4),
+  _______,  KC_INS,  QK_MOUSE_BUTTON_1,   QK_MOUSE_CURSOR_UP,  QK_MOUSE_BUTTON_2, MS_WHLU,                            KC_PGUP, KC_PRVWD,   KC_UP, KC_NXTWD,C(KC_BSPC), KC_BSPC,
+  C(KC_Y), C(KC_A),  QK_MOUSE_CURSOR_LEFT,  QK_MOUSE_CURSOR_DOWN,  QK_MOUSE_CURSOR_RIGHT, MS_WHLD,                  KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
   _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XXXXXXX,  _______,                                          _______,  XXXXXXX, KC_LSTRT, XXXXXXX, KC_LEND,   XXXXXXX, _______,
                          _______, _______, _______, _______, _______,                                    _______, _______, _______, _______, _______
 ),
@@ -105,8 +105,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 
-[_Func] =  LAYOUT(KC_NO, KC_NUM, KC_P7, KC_P8, KC_P9, KC_PSLS, KC_NUM, KC_NO, KC_NO, KC_PSLS, KC_NO, KC_NO, KC_NO, KC_BSPC, KC_P4, KC_P5, KC_P6, KC_PAST, KC_P7, KC_P8, KC_P9, KC_PAST, KC_NO, KC_TRNS, KC_NO, KC_DEL, KC_P1, KC_P2, KC_P3, KC_PMNS, KC_P4, KC_P5, KC_P6, KC_PMNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_P0, KC_PDOT, KC_PENT, KC_PPLS, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_PPLS, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_PENT, KC_NO, KC_NO, KC_P0, KC_PDOT, KC_PENT, KC_NO),
-[_Syntax] =  LAYOUT(KC_NO, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_MRWD, KC_MPLY, KC_MFFD, MS_WHLL, MS_WHLU, MS_WHLR, KC_NO, KC_NO, KC_TRNS, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, UG_TOGG, UG_TOGG, KC_NO, MS_WHLD, KC_NO, KC_NO, KC_NO, KC_RSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+[_Func] = LAYOUT(KC_NO, KC_NUM, KC_P7, KC_P8, KC_P9, KC_PSLS, KC_NUM, KC_NO, KC_NO, KC_PSLS, KC_NO, KC_NO, KC_NO, KC_BSPC, KC_P4, KC_P5, KC_P6, KC_PAST, KC_P7, KC_P8, KC_P9, KC_PAST, KC_NO, KC_TRNS, KC_NO, KC_DEL, KC_P1, KC_P2, KC_P3, KC_PMNS, KC_P4, KC_P5, KC_P6, KC_PMNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_P0, KC_PDOT, KC_PENT, KC_PPLS, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_PPLS, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_PENT, KC_NO, KC_NO, KC_P0, KC_PDOT, KC_PENT, KC_NO),
+[_Syntax] = LAYOUT(KC_NO, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, MS_WHLL, MS_WHLU, MS_WHLR, KC_NO, KC_NO, KC_TRNS, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, UG_TOGG, UG_TOGG, KC_NO, MS_WHLD, KC_NO, KC_NO, KC_NO, KC_RSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
 };
 
 
