5fd4bd5d99c84ef76517973118555fdc1c9f4f4a
unknown <ddat888@gmail.com>
2025-11-09 11:13:51 +0700

    change keymap

--- keyboards/sofle/keymaps/via/keymap.c
+++ keyboards/sofle/keymaps/via/keymap.c
@@ -20,15 +20,27 @@ enum custom_keycodes {
     KC_WTAB,     // Win+Tab (task view)
     KC_WNEWDESK,  // Win+Ctrl+D (create new virtual desktop)
     KC_WDELDESK, // Win+Ctrl+F4 (delete current virtual desktop)
+    KC_DELLINE,
 };
 
+enum {
+    TD_ESC_CAPS,
+};
+
+// Tap Dance definitions
+// tap_dance_action_t tap_dance_actions[] = {
+//     // Tap once for Escape, twice for Caps Lock
+//     [TD_ESC_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS),
+// };
 const uint16_t PROGMEM esc[] = {KC_GRV, KC_1, COMBO_END};
 const uint16_t PROGMEM func[] = {KC_LGUI, KC_LALT, KC_LCTL, COMBO_END};
 const uint16_t PROGMEM equal[] = {KC_J, KC_K, COMBO_END};
-const uint16_t PROGMEM left_brace[] = {KC_D, KC_F, COMBO_END};
-const uint16_t PROGMEM right_brace[] = {KC_F, KC_G, COMBO_END};
-const uint16_t PROGMEM left_bracket[] = {KC_C, KC_V, COMBO_END};
-const uint16_t PROGMEM right_bracket[] = {KC_V, KC_B, COMBO_END};
+const uint16_t PROGMEM left_brace[] = {KC_C, KC_V, COMBO_END};
+// const uint16_t PROGMEM right_brace[] = {KC_V, KC_B, COMBO_END};
+const uint16_t PROGMEM right_brace[] = {C(KC_C), C(KC_V), COMBO_END};
+const uint16_t PROGMEM left_bracket[] =  {KC_D, KC_F, COMBO_END};
+// const uint16_t PROGMEM right_bracket[] =  {KC_F, KC_G, COMBO_END};
+const uint16_t PROGMEM right_bracket[] = {QK_MOUSE_CURSOR_DOWN,  QK_MOUSE_CURSOR_RIGHT, COMBO_END};
 const uint16_t PROGMEM backslash[] = {KC_M, KC_COMM, COMBO_END};
 
 combo_t key_combos[] = {
@@ -84,10 +96,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_Nav] = LAYOUT(
-  _______, A(KC_TAB) , KC_WDESK_L, KC_WTAB,  KC_WDESK_R , KC_WNEWDESK,                                                     KC_WNEWDESK,KC_WDESK_L, KC_WTAB,  KC_WDESK_R,  KC_WDELDESK ,A(KC_F4),
+  _______, KC_WDELDESK , KC_WDESK_L, KC_WTAB,  KC_WDESK_R , KC_WNEWDESK,                                                     KC_WNEWDESK,KC_WDESK_L, KC_WTAB,  KC_WDESK_R,  KC_WDELDESK ,A(KC_F4),
   _______,  KC_INS,  QK_MOUSE_BUTTON_1,   QK_MOUSE_CURSOR_UP,  QK_MOUSE_BUTTON_2, MS_WHLU,                            KC_PGUP, KC_PRVWD,   KC_UP, KC_NXTWD,C(KC_BSPC), KC_BSPC,
-  C(KC_Y), C(KC_A),  QK_MOUSE_CURSOR_LEFT,  QK_MOUSE_CURSOR_DOWN,  QK_MOUSE_CURSOR_RIGHT, MS_WHLD,                  KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
-  _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XXXXXXX,  _______,                                          _______,  XXXXXXX, KC_LSTRT, XXXXXXX, KC_LEND,   XXXXXXX, _______,
+  C(KC_Y), C(KC_A),  QK_MOUSE_CURSOR_LEFT,  QK_MOUSE_CURSOR_DOWN,  QK_MOUSE_CURSOR_RIGHT, MS_WHLD,                  KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_DELLINE,
+  _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), C(S(KC_DOT)),  _______,                                          _______,  XXXXXXX, KC_LSTRT, XXXXXXX, KC_LEND,   XXXXXXX, _______,
                          _______, _______, _______, _______, _______,                                    _______, _______, _______, _______, _______
 ),
 /* Func
@@ -104,6 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
+
 
 [_Func] = LAYOUT(KC_NO, KC_NUM, KC_P7, KC_P8, KC_P9, KC_PSLS, KC_NUM, KC_NO, KC_NO, KC_PSLS, KC_NO, KC_NO, KC_NO, KC_BSPC, KC_P4, KC_P5, KC_P6, KC_PAST, KC_P7, KC_P8, KC_P9, KC_PAST, KC_NO, KC_TRNS, KC_NO, KC_DEL, KC_P1, KC_P2, KC_P3, KC_PMNS, KC_P4, KC_P5, KC_P6, KC_PMNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_P0, KC_PDOT, KC_PENT, KC_PPLS, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_PPLS, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_PENT, KC_NO, KC_NO, KC_P0, KC_PDOT, KC_PENT, KC_NO),
 [_Syntax] = LAYOUT(KC_NO, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, MS_WHLL, MS_WHLU, MS_WHLR, KC_NO, KC_NO, KC_TRNS, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, UG_TOGG, UG_TOGG, KC_NO, MS_WHLD, KC_NO, KC_NO, KC_NO, KC_RSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
@@ -230,6 +243,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 unregister_mods(mod_config(MOD_LGUI | MOD_LCTL));
                 unregister_code(KC_F4);
             }
+            break;
+        case KC_DELLINE:
+            if (record->event.pressed) {
+                tap_code(KC_HOME);
+                register_code(KC_LSFT);
+                tap_code(KC_END);
+                unregister_code(KC_LSFT);
+                tap_code(KC_DEL);
+            }
+            return false;
+            break;
     }
     return true;
 }
